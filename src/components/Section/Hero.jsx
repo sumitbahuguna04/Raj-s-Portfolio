d85fceec3b0fe41275f0479cb6fcb9533b7964c9
sumitbahuguna04 <sumitbahuguna768@gmail.com>
2025-07-28 21:59:53 +0530

    changed

--- src/components/Section/Hero.jsx
+++ src/components/Section/Hero.jsx
@@ -2,31 +2,30 @@ import React from "react";
 
 function Hero() {
   return (
-    <div className="w-full mt-10 bg-[#19191d] text-white shadow-lg p-6 flex flex-col md:flex-row items-center md:items-start gap-6">
-      
-      {/* Left side - User Data (40%) */}
-      <div className="md:w-[40%] mb-5 px-4 text-xl sm:text-2xl md:text-4xl">
-        <h2 className="text-2xl font-bold mb-3">Mr Rays</h2>
-        <p className="text-[#b0b0b0] mb-4">
-          A passionate <span className="text-[#8a88f1] font-semibold">Fitness Trainer & Yoga Instructor</span> with 10+ years of experience helping people achieve their health goals.
-        </p>
-        <p className="text-[#d0d0d0] mt-10">
-          I specialize in creating personalized training programs combining strength, flexibility, and mindfulness techniques to improve overall well-being. My mission is to help individuals find balance between body and mind.
-        </p>
-      </div>
+    <div className="max-w-7xl mx-auto sm:px-6 lg:px-12 mt-10">
+      <div className="w-full bg-[#19191d] text-white rounded-3xl shadow-lg p-6 sm:p-10 flex flex-col lg:flex-row items-center lg:items-start gap-8">
+        
+        {/* Left side - User Data */}
+        <div className="w-full lg:w-[40%] flex flex-col lg:m-20 lg:gap-4 text-center lg:text-left">
+          <h2 className="font-bold mb-4 text-4xl">Mr Rays</h2>
+          <p className="text-[#b0b0b0] text-xl sm:text-2xl md:text-3xl mb-6">
+            <span className="text-[#8a88f1] font-semibold">Fitness Trainer & Yoga Instructor</span> with 10+ years of experience.
+          </p>
+          <p className="text-[#d0d0d0] text-sm sm:text-base md:text-lg">
+            I create simple, effective training plans to help you stay strong, flexible, and balanced.
+          </p>
+        </div>
 
-      {/* Right side - User Image (60%) */}
-      <div className="md:w-[60%] flex justify-center text-xl md:text-2xl">
-        <img
-          src="raysimg.jpg"
-          alt="User"
-          className="rounded-2xl w-full max-w-[500px] h-auto object-cover border-2 border-[#2e2e30]"
-        />
+        {/* Right side - User Image */}
+        <div className="w-full lg:w-[60%] flex justify-center">
+          <img
+            src="raysimg.jpg"
+            alt="User"
+            className="rounded-2xl w-full max-w-[400px] sm:max-w-[500px] h-auto object-cover border-2 border-[#2e2e30]"
+          />
+        </div>
       </div>
-
     </div>
-  
-    
   );
 }
 
