aa86ec8a5b03b18e519ddc265952637d4635a7bb
sumitbahuguna04 <sumitbahuguna768@gmail.com>
2025-07-28 22:21:57 +0530

    changes

--- src/components/Section/Navbar.jsx
+++ src/components/Section/Navbar.jsx
@@ -2,17 +2,17 @@ import React from "react";
 
 function Navbar() {
   return (
-    <div className="sm:mt-10 w-[100%] sm:w-[85%] md:w-[75%] lg:w-[60%] px-4 sm:px-6 md:px-10 bg-[#19191d] border border-[#2e2e30] hover:border-[#484374] transition duration-500 text-white mx-auto flex items-center justify-center sm:gap-5 md:gap-8 rounded-3xl py-3 sm:py-4 shadow-lg">
-      <div className="hover:bg-[#31313889] rounded-2xl px-2 sm:px-4 py-1 text-center transition-colors duration-300 ease-in-out">
-        <a href="">Home</a>
+    <div className="sm:mt-10 w-[100%] sm:w-[85%] md:w-[75%] lg:w-[60%] px-4 sm:px-6 md:px-10 bg-[#151517] border border-[#2e2e30] hover:border-[#484374] transition duration-500 text-white mx-auto flex items-center justify-center sm:gap-5 md:gap-8 rounded-3xl py-3 sm:py-4 shadow-lg">
+      <div className=" rounded-2xl  sm:px-4  text-center  ease-in-out">
+        <a href="" className="px-4 py-1 rounded-4xl hover:bg-[#272a2d] transition-colors duration-300">Home</a>
       </div>
-      <div className="hover:bg-[#31313889] rounded-2xl px-2 sm:px-4 py-1 text-center transition-colors duration-300 ease-in-out">
+      <div className="px-4 py-1 rounded-4xl hover:bg-[#272a2d] transition-colors duration-300">
         <a href="">About</a>
       </div>
-      <div className="hover:bg-[#31313889] rounded-2xl px-2 sm:px-4 py-1 text-center transition-colors duration-300 ease-in-out">
+      <div className="px-4 py-1 rounded-4xl hover:bg-[#272a2d] transition-colors duration-300">
         <a href="">Services</a>
       </div>
-      <div className="hover:bg-[#31313889] rounded-2xl px-2 sm:px-4 py-1 text-center transition-colors duration-300 ease-in-out">
+      <div className="px-4 py-1 rounded-4xl hover:bg-[#272a2d] transition-colors duration-300">
         <a href="">Contact</a>
       </div>
     </div>
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
+import "@fontsource/poppins/300.css";
+import "@fontsource/poppins/400.css";
+import "@fontsource/poppins/500.css";
+import "@fontsource/poppins/600.css";
+import "@fontsource/poppins/700.css";
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
