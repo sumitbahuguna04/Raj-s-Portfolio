d48e0b54bc7194bdd50450316e42dd093757ebb1
sumitbahuguna04 <sumitbahuguna768@gmail.com>
2025-07-29 11:42:32 +0530

    changed

--- src/components/Section/Hero.jsx
+++ src/components/Section/Hero.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 function Hero() {
   return (
-    <div className="max-w-7xl mx-auto sm:px-6 lg:px-12 mt-10 mb-20">
-      <div className="w-full bg-[#19191d] text-white rounded-3xl shadow-lg p-6 sm:p-10 flex flex-col lg:flex-row items-center lg:items-start gap-8">
+   <div className= 'max-w-[1600px] mx-auto bg-[#19191d]   rounded-lg shadow-lg mt-10 p-2 sm:p-3 md:p-5 lg:p-10'>
+      <div className="w-full  text-white rounded-3xl p-6 sm:p-10 flex flex-col lg:flex-row items-center lg:items-start gap-8">
         
         {/* Left side - User Data */}
         <div className="w-full lg:w-[40%] flex flex-col lg:m-20 lg:gap-4 text-center lg:text-left">
