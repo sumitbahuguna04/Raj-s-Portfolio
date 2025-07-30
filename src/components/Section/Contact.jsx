08205db3dff4864998305a23a2ed3731b7ff9e26
sumitbahuguna04 <sumitbahuguna768@gmail.com>
2025-07-30 17:52:19 +0530

    contact link add

--- src/components/Section/Contact.jsx
+++ src/components/Section/Contact.jsx
@@ -9,42 +9,54 @@ function Contact({ lang }) {
         <h1 className="text-orange-300 mt-5  round text-2xl sm:text-4xl  mb-2 sm:mb-3 lg:mb-5 text-center">
           {content.title}
         </h1>
-        <div className="w-full bg-[#b88f557d] p-[0.5px]"></div>{" "}
-        <div className="flex  justify-evenly items-center mt-3 sm:mt-5 md:mt-10 p-2 sm:p-3 md:p-5 ">
-          <div className="flex flex-col  gap-3 sm:gap-4">
-            <a href="" target="_blank" rel="noopener noreferrer">
+        <div className="w-full bg-[#b88f557d] p-[0.5px]"></div>
+        <div className="flex  justify-evenly items-center mt-3 sm:mt-5 md:mt-7 p-2 sm:p-3 md:p-5 ">
+          <div className="flex flex-col items-center gap-3 sm:gap-4">
+            <div className="w-full flex justify-center">
               <img
                 src="weChat.jpg"
                 alt=""
-                className="size-12 hover:scale-110 transition duration-500 md:size-14 rounded-2xl "
+                className="size-12 md:size-14 rounded-2xl hover:scale-110 transition duration-500"
               />
-            </a>
-            <h1>{content.icon1}</h1>
+            </div>
+            <div className="text-center space-y-1">
+              <h1>{content.icon1} :</h1>
+              <a className="underline text-[#fee1ab] hover:text-[#fee1abc1] cursor-pointer transition ">
+                Rayskol365
+              </a>
+            </div>
           </div>
-          <div className="flex flex-col gap-2 sm:gap-3">
-            <a href="" target="_blank" rel="noopener noreferrer">
+
+          <div className="flex flex-col items-center gap-3 sm:gap-4">
+            <div className="w-full flex justify-center">
               <img
                 src="tiktok.webp"
-                alt="TikTok"
-                className="size-13 hover:scale-110 transition duration-500 md:size-15 rounded-2xl"
+                alt=""
+                className="size-12 md:size-14 rounded-2xl hover:scale-110 transition duration-500"
               />
-            </a>
-
-            <h1>{content.icon2}</h1>
+            </div>
+            <div className="text-center space-y-1">
+              <h1>{content.icon2} :</h1>
+              <a className="underline text-[#fee1ab] hover:text-[#fee1abc1] cursor-pointer transition ">
+                Rayskol365
+              </a>
+            </div>
           </div>
-          <div className="flex flex-col gap-2 sm:gap-3">
-            <a
-              href="http://xhslink.com/m/8XlntkXFhBx "
-              target="_blank"
-              rel="noopener noreferrer"
-            >
+
+          <div className="flex flex-col items-center gap-3 sm:gap-4">
+            <div className="w-full flex justify-center">
               <img
                 src="redNote.jpg"
                 alt=""
-                className="size-13 hover:scale-110 transition duration-500  md:size-15 rounded-2xl"
+                className="size-12 md:size-14 rounded-2xl hover:scale-110 transition duration-500"
               />
-            </a>
-            <h1>{content.icon3}</h1>
+            </div>
+            <div className="text-center space-y-1">
+              <h1>{content.icon3} :</h1>
+              <a className="underline text-[#fee1ab] hover:text-[#fee1abc1] cursor-pointer transition duration-300">
+                Rayskol365
+              </a>
+            </div>
           </div>
         </div>
       </div>
--- src/components/Section/Events.jsx
+++ src/components/Section/Events.jsx
@@ -4,65 +4,60 @@ import React from "react";
 function Events({ lang }) {
   const content = eventsContent[lang];
   return (
-    <div id="events" className=" max-w-[1600px] mx-auto bg-[#202025b6] rounded-2xl  mt-10 p-2 sm:p-3 md:p-5 lg:p-10">
+    <div
+      id="events"
+      className=" max-w-[1600px] mx-auto bg-[#202025b6] rounded-2xl  mt-10 p-2 sm:p-3 md:p-5 lg:p-10"
+    >
       <h1 className="text-orange-300 mt-5  round text-2xl sm:text-4xl  mb-2 sm:mb-3 lg:mb-5 text-center">
         {content.title}
       </h1>
       <div className="w-full bg-[#b88f557d] p-[0.5px]"></div>
       <div className="mt-10 lg:p-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
-        <div className="">
+        <div>
           <img
             src="award.jpg"
-            alt="User"
             className="rounded-2xl w-full h-auto object-cover border-2 border-[#2e2e30]"
           />
         </div>
         <div>
           <img
             src="award1.jpg"
-            alt="User"
             className="rounded-2xl w-full h-auto object-cover border-2 border-[#2e2e30]"
           />
         </div>
         <div>
           <img
             src="event9.jpg"
-            alt="User"
             className="rounded-2xl w-full h-auto object-cover border-2 border-[#2e2e30]"
           />
         </div>
-         <div>
+        <div>
           <img
             src="event6.jpg"
-            alt="User"
             className="rounded-2xl w-full h-auto object-cover border-2 border-[#2e2e30]"
           />
         </div>
-         <div>
+        <div>
           <img
             src="event7.jpg"
-            alt="User"
             className="rounded-2xl w-full h-auto object-cover border-2 border-[#2e2e30]"
           />
         </div>
-         <div>
+        <div>
           <img
             src="event8.jpg"
-            alt="User"
             className="rounded-2xl w-full h-auto object-cover border-2 border-[#2e2e30]"
           />
         </div>
-         <div>
+        <div className="">
           <img
             src="event9.jpg"
-            alt="User"
             className="rounded-2xl w-full h-auto object-cover border-2 border-[#2e2e30]"
           />
         </div>
-         <div>
+        <div className="">
           <img
             src="event10.jpg"
-            alt="User"
             className="rounded-2xl w-full h-auto object-cover border-2 border-[#2e2e30]"
           />
         </div>
