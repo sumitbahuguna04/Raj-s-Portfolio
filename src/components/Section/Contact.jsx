615879b7d9d185b35693242304bc3e2b7829750a
sumitbahuguna04 <sumitbahuguna768@gmail.com>
2025-07-30 20:55:13 +0530

    icon updated

--- src/components/Section/Contact.jsx
+++ src/components/Section/Contact.jsx
@@ -4,63 +4,73 @@ import React from "react";
 function Contact({ lang }) {
   const content = contactContent[lang];
   return (
-    <>
-      <div className="max-w-[1600px] mx-auto bg-[#202025b6] text-white rounded-2xl  mt-10 p-2 sm:p-3 md:p-5 lg:p-10 mb-10">
-        <h1 className="text-orange-300 mt-5  round text-2xl sm:text-4xl  mb-2 sm:mb-3 lg:mb-5 text-center">
-          {content.title}
-        </h1>
-        <div className="w-full bg-[#b88f557d] p-[0.5px]"></div>
-        <div className="flex  justify-evenly items-center mt-3 sm:mt-5 md:mt-7 p-2 gap-5 sm:p-3 md:p-5 ">
-          <div className="flex flex-col items-center gap-3 sm:gap-4">
-            <div className="w-full flex justify-center">
-              <img
-                src="weChat.jpg"
-                alt=""
-                className="size-12 md:size-14 rounded-2xl hover:scale-110 transition duration-500"
-              />
-            </div>
-            <div className="text-center space-y-1">
-              <h1>{content.icon1} :</h1>
-              <a className="underline text-xs  sm:text-[15px]   text-[#fee1ab] hover:text-[#fee1abc1] cursor-pointer transition ">
-                Rayskol365
-              </a>
-            </div>
-          </div>
+    <div className="max-w-[1600px] mx-auto bg-[#202025b6] text-white rounded-2xl mt-10 p-2 sm:p-3 md:p-5 lg:p-10 mb-10">
+      <h1 className="text-orange-300 mt-5 round text-2xl sm:text-4xl mb-2 sm:mb-3 lg:mb-5 text-center">
+        {content.title}
+      </h1>
+      <div className="w-full bg-[#b88f557d] p-[0.5px]"></div>
 
-          <div className="flex flex-col items-center gap-3 sm:gap-4">
-            <div className="w-full flex justify-center">
-              <img
-                src="tiktok.webp"
-                alt=""
-                className="size-12 md:size-14 rounded-2xl hover:scale-110 transition duration-500"
-              />
-            </div>
-            <div className="text-center space-y-1">
-              <h1>{content.icon2} :</h1>
-              <a className="underline text-xs  sm:text-[15px] text-[#fee1ab] hover:text-[#fee1abc1] cursor-pointer transition ">
-                Rayskol365
-              </a>
-            </div>
-          </div>
+      <div className="mt-5 grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-5 sm:gap-8 p-2 sm:p-3 md:p-5">
+        {/* Item 1 */}
+        <div className="flex flex-col items-center gap-2 sm:gap-3">
+          <img
+            src="weChat.jpg"
+            alt=""
+            className="size-12 md:size-16 rounded-2xl hover:scale-110 transition duration-500"
+          />
+          <a className="underline text-xs sm:text-sm md:text-base text-[#fee1ab] hover:text-[#fee1abc1] transition">
+            Rayskol365
+          </a>
+        </div>
 
-          <div className="flex flex-col items-center gap-3 sm:gap-4">
-            <div className="w-full flex justify-center">
-              <img
-                src="redNote.jpg"
-                alt=""
-                className="size-12 md:size-14 rounded-2xl hover:scale-110 transition duration-500"
-              />
-            </div>
-            <div className="text-center space-y-1">
-              <h1>{content.icon3} :</h1>
-              <a className="underline text-xs  sm:text-[15px]  text-[#fee1ab] hover:text-[#fee1abc1] cursor-pointer transition duration-300">
-                Rayskol365
-              </a>
-            </div>
-          </div>
+        {/* Item 2 */}
+        <div className="flex flex-col items-center gap-2 sm:gap-3">
+          <img
+            src="tiktok.webp"
+            alt=""
+            className="size-12 md:size-16 rounded-2xl hover:scale-110 transition duration-500"
+          />
+          <a
+            href="https://www.douyin.com/user/MS4wLjABAAAADfoIyAjylnJFt372NiDc6U-uoUPwb97i0XkGsDg0UT8?from_tab_name=main&vid=7453474235378453818"
+            className="underline text-xs sm:text-sm md:text-base text-[#fee1ab] hover:text-[#fee1abc1] transition"
+          >
+            雷力士Rays
+          </a>
+        </div>
+
+        {/* Item 3 */}
+        <div className="flex flex-col items-center gap-2 sm:gap-3">
+          <img
+            src="redNote.jpg"
+            alt=""
+            className="size-12 md:size-16 rounded-2xl hover:scale-110 transition duration-500"
+          />
+          <a
+            href="http://www.xiaohongshu.com/user/profile/5cd500740000000012011aa1?xsec_token=YBmPEX9RkX1gnFpLZuyrFrM5hxYS13PUuSN9_uetcr6n0%3D&xsec_source=app_share&xhsshare=CopyLink&appuid=5cd500740000000012011aa1&apptime=1736932860&share_id=8ed088f6014d4f21a09a7298a5f434d1"
+            className="underline text-xs sm:text-sm md:text-base text-[#fee1ab] hover:text-[#fee1abc1] transition"
+          >
+            雷力士Rays
+          </a>
+        </div>
+
+        {/* Item 4 */}
+        <div className="flex flex-col items-center gap-2 sm:gap-3">
+          <img
+            src="insta.jpg"
+            alt=""
+            className="size-12 md:size-16 rounded-2xl hover:scale-110 transition duration-500"
+          />
+          <a
+            href="https://instagram.com/iamrays365?igsh=MThzNzd1cjRyZGMyYw=="
+            target="_blank"
+            rel="noopener noreferrer"
+            className="underline text-xs sm:text-sm md:text-base text-[#fee1ab] hover:text-[#fee1abc1] transition"
+          >
+            iamrays365
+          </a>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
--- src/data/contactContent.js
+++ src/data/contactContent.js
@@ -5,12 +5,15 @@ const contactContent = {
     icon1: "We Chat",
     icon2: "Tik Tok",
     icon3: "Red Note",
+    icon4: "Instagram",
+
   },
   zh: {
     title: "联系",
     icon1: "微信",
     icon2: "抖音",
     icon3: "小红书",
+    icon4: "Instagram",
   },
 };
 
