41edc9b2dcb5dbad0d041805744d3fae3c7afb19
sumitbahuguna04 <sumitbahuguna768@gmail.com>
2025-07-30 16:24:37 +0530

    ..

--- src/components/Section/Hero.jsx
+++ src/components/Section/Hero.jsx
@@ -6,7 +6,7 @@ function Hero({ lang }) {
   const content = heroContent[lang];
 
   return (
-    <div className='max-w-[1600px] mx-auto bg-[#202025] rounded-lg shadow-lg mt-10 p-2 sm:p-3 md:p-5 lg:p-10'>
+    <div className='max-w-[1600px] mx-auto bg-[#202025b6] rounded-lg shadow-lg mt-10 p-2 sm:p-3 md:p-5 lg:p-10'>
       <div className="w-full text-white rounded-3xl p-6 sm:p-10 flex flex-col lg:flex-row items-center lg:items-start gap-8">
         
         {/* Left side - User Data */}
